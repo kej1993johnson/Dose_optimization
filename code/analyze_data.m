7f3aaabf2eafaee8f27087e5684b65831126d1c5
Kaitlyn <kj22643@wireless-10-147-173-172.public.utexas.edu>
2019-01-31 10:02:13 -0600

    plot additional parameter outputs

--- code/analyze_data.m
+++ code/analyze_data.m
@@ -40,3 +40,39 @@ for i =1:length(traj)
     title('Resistant regrowth rate versus WPT')
     end
 end
+
+%% Plot comparing number of doses
+% Just playing around here
+
+figure;
+subplot(1,3,1)
+for i = 1:length(traj)
+    if ~isempty(traj(i).numdoses) && traj(i).bfmod == 1
+    plot(traj(i).numdoses, traj(i).phi, 'r*')
+    hold on
+    xlabel('number of doses received')
+    ylabel ('resistant fraction (\phi)')
+    title('Resistant fraction versus number of doses')
+    end
+end
+subplot(1,3,2)
+for i =1:length(traj)
+    if ~isempty(traj(i).numdoses) && traj(i).bfmod == 1
+    plot(traj(i).numdoses, traj(i).k, 'b*')
+    hold on
+    xlabel('number of doses recevied')
+    ylabel ('death rate (k)')
+    title('Death rate versus number of doses')
+    end
+end
+
+subplot(1,3,3)
+for i =1:length(traj)
+    if ~isempty(traj(i).numdoses) && traj(i).bfmod == 1
+    plot(traj(i).numdoses, traj(i).g, 'g*')
+    hold on
+    xlabel('weeks post prior treatment')
+    ylabel ('resistant regrowth rate (g)')
+    title('Resistant growth rate versus number of doses')
+    end
+end
