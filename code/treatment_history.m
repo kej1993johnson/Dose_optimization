83bdbf7c06baa2dc4375682b50172e525e2056c8
William Mo <wlmtmsm@utexas.edu>
2019-06-21 12:39:54 -0500

    reworked vector generation for graphing

--- code/treatment_history.m
+++ code/treatment_history.m
@@ -8,10 +8,12 @@ close all; clear all; clc;
 %% Load in data structure 
 S = load('../out/trajraw.mat');
 traj= S.traj;
-%% Start with an example for 2 repeat treatments 3 weeks apart at 75 nM each
-% this corresponds to the 32th row in the structure
-i = 32;
+%% pick well (row in traj) to graph
+i = 58;
 
+
+%% following block is Kaitlyn's old time and dosage vectors
+%{
 % Start my making a time vector that spans the pre-treatment history + two
 % weeks into the current treatment. So here that will be
 tvec = 0:1:(7*(sum(traj(i).doseints)+2));
@@ -29,9 +31,31 @@ for j = 1:length(traj(i).doseints)
     ind = find(ismember(tvec, (traj(i).doseints(j)*7):((traj(i).doseints(j)*7)+traj(i).doseduration/24)));
     Uvec(ind)= traj(i).prevdose(j);
 end
-
-figure;
-plot(tvec, Uvec, 'b-', 'LineWidth', 3)
+%}
+%% time and dosage vectors to plot
+% generates tvec truncated after last dose
+tvec = 0:1:(sum(traj(i).doseintdays) + 2 + (3 * traj(i).numdoses));
+% equalize vector length of y values
+uvec = 0.*tvec;
+n = traj(i).numdoses;
+% see "dosegraphindex.pdf" in documentation to understand this index jumping/modifying
+ind = 4;
+for j = 1:n
+    for k = ind:length(tvec)
+        tvec(k) = tvec(k) - 1;
+    end
+    for k = (ind + 2):length(tvec)
+        tvec(k) = tvec(k) - 1;
+    end
+    uvec(ind) = traj(i).dose;
+    uvec(ind + 1) = traj(i).dose;
+    if j ~= n
+        ind = ind + 3 + traj(i).doseintdays(j);
+    end
+end    
+%% plot and format
+plot(tvec, uvec)
+uvec;
 xlabel('time (days)')
 ylabel('Dose')
 title('Treatment History Example')
